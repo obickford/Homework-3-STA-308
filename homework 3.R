b5e1400ffa5d5b8ccd050434167912cbb159771d
obickford <bickfoor@miamioh.edu>
2022-04-07 20:36:50 -0400

    finished and successful question 1 and 2, now need to somehow figure out number 3

--- homework 3.R
+++ homework 3.R
@@ -1,6 +1,7 @@
 ## Question 1
 
-takeOneTest <- function(probsGood = 0.5) {
+takeOneTest <- function(probsGood = 0.6) {
+  my_score <- 0
   my_mood <- sample(c("Good", "Bad"),
                     size = 1, 
                     prob = c(probsGood, 1-probsGood) ) 
@@ -9,11 +10,30 @@ takeOneTest <- function(probsGood = 0.5) {
                        size =1,
                        prob = c(0.8, 0.2))
   } else {
+    
+  }
+  if (my_mood == "Bad") {
     my_score <- sample(c(100, 75, 50),
                        size = 1,
                        prob = c(0.2, 0.3, 0.5))
   }
-  my_score
+  print(my_score)
 }
-takeOneTest(probsGood = 0.6)
+takeOneTest()
+
+############################################################
+## Question 2
+
+SemesterGrades <- function(num_tests) {
+  for (i in 1:num_tests) {
+    takeOneTest()
+  }
+}
+SemesterGrades2(5)
+
+############################################################
+## Question 3
+
+
+
 
