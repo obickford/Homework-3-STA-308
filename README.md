051ecede240ac45ecb4327f84aaadac79d2b9403
obickford <bickfoor@miamioh.edu>
2022-04-10 17:15:45 -0400

    finished code

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Homework-3
+
+I created a function with a for() loop called avg_test that used the SemesterGrades function and allowed the user to specify the number of tests. It randomly sampled the num_tests through the SemesterGrades function and saved it as my_test then took the average of my_test.By have avg_test, this allowed me to test it in a for() loop for 3 tests and 12 tests and just change the num_tests accordingly and compare the stats and distributions. 
+
+
+When running the summary function for an average of 3 tests compared to an average of 12 tests, 3 tests had an overall lower minimum score and a slightly lower median and mean. Sometimes the mean of 3 tests would be higher but only by a point or less. The max score was always higher but the standard deviation was larger. When running the histogram it was slightly left skewed and had a wider range/spread. 
+
+With an average of 12 tests, the mean and median were slightly higher for most samples. It always had a higher minimum value, but a lower maximum value than 3 tests. It is unimodal and bell shaped with a low standard deviation. 
+
+
+I would rather have 3 tests rather than 12. The mean and median were very similar and often times the same. The maximum score for three tests was always higher than 12, but there is a lower minimum score however the distribution is left skewed so there will be many more higher averages then there will be of the low and close to that minimum score. The 3 tests also had scores between 90-100 whereas the 12 has a max of 90.80. While there is more spread, the averages were the same and I prefer the left skewness of the 3 tests and having the ability to score higher and have a higher maximum. 
--- homework 3.R
+++ homework 3.R
@@ -1,16 +1,42 @@
+#####################
+##  
+##  Olivia Bickford
+##  April 10, 2022
+##  Homework #3
+##
+##    Working more with practice on functions and applying them to probability
+##    and applications related to basic statistics such as...
+##    weighted probability, random sampling, 5 statistic summary,
+##    histograms, describing graphs, simulations
+##
+##    Used functions to step through a probability scenario of having a good or bad mood
+##    and depending on the mood determining the probability of what test grade you
+##    would randomly receive on a random day
+##
+##    Later combined the functions to determine averages and how many tests you would want
+##    to take by comparing the stats of the random sampling and histograms
+##
+##    Also worked on using functions - ifelse(), sample(), for()
+##    and used new technique with for() loop
+##    by saving results into a new vector and including the vector within the function
+##    Added parameters in the function to allow the user to specify what they want
+##      In this example we used num_tests for the user to put in the
+##      the number of tests they wanted
+##
+########################
 ## Question 1
 
 takeOneTest <- function(probsGood = 0.6) {
-  my_mood <- sample(c("Good", "Bad"),
+  my_mood <- sample(c("Good", "Bad"),  ##sampling for the good and bad day probs
                     size = 1, 
                     prob = c(probsGood, 1-probsGood) ) 
   
   if (my_mood == "Good") {
-    my_score <- sample(c(100, 75),
+    my_score <- sample(c(100, 75), ## if the prob is good sample these conditions
                        size =1,
                        prob = c(0.8, 0.2))
   } else {
-    my_score <- sample(c(100, 75, 50),
+    my_score <- sample(c(100, 75, 50), ## else (if its bad) sample these conditions
                          size = 1,
                          prob = c(0.2, 0.3, 0.5))
   }
@@ -23,12 +49,12 @@ takeOneTest()
 ############################################################
 ## Question 2
 
-SemesterGrades <- function(num_tests) {
+SemesterGrades <- function(num_tests) {  
   my_sem_grades <- c()
   
-  for (i in 1:num_tests) {
+  for (i in 1:num_tests) {  ##had to set parameters to num_tests so the user can specify the number
    my_sem_grades <- c(my_sem_grades,
-                      takeOneTest())
+                      takeOneTest()) ##uses the function from question 1 to have the same samples
   }
   my_sem_grades
 }
@@ -39,31 +65,30 @@ SemesterGrades(6)
 
 avg_test <- function(num_tests=1) {
   my_test <- c()
-  for(i in 1:num_tests) {
+  for(i in 1:num_tests) {  
     my_test <- c(my_test,
-                 SemesterGrades(num_tests))
+                 SemesterGrades(num_tests)) ##using a function with the SemesterGrade to specify num_tests
   }
-  mean(my_test)
+  mean(my_test) ##takes mean of the for() loop
 }
 
 avg_test(num_tests = 6)
+## Means ranged from 79.16667 to 85.41667 and 81.25 and 84.72222 when sampled for 6
 
-## Means ranged from 79.16667 to 85.41667 and 81.25 and 84.72222
-######################################################
-set.seed(1)
+
+##Average for 3 tests using the basis of avg_test
+
 myAvgTest3 <- c()
 for(i in 1:10000) {
   myAvgTest3 <- c(myAvgTest3,
                     avg_test(num_tests = 3))
 }
 summary(myAvgTest3)
-##lower minimum score, lower median and mean, higher max score but more wider standard deviation
 hist(myAvgTest3)
-## left skewed, wider range/spread
 sd(myAvgTest3)
-## sd= 6.586646
 
-########################################################
+
+## Average for 12 tests using the basis of avg_test
 
 myAvgTest12 <- c()
 for(i in 1:10000) {
@@ -71,10 +96,6 @@ for(i in 1:10000) {
                   avg_test(num_tests = 12))
 }
 summary(myAvgTest12)
-##higher min value and overall mean and median score, but lower max value, 
-##  standard deviation is a lot lower as well
 hist(myAvgTest12)
-## more unimodal and bell shaped
 sd(myAvgTest12)
-## sd= 1.656258
 
